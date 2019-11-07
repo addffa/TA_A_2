8be3dae72c479260ded68cc0291272733bef80bd
Anastasia Greta <agretaelena@gmail.com>
2019-11-07 14:26:07 +0700

    feat: add repository JenisBukuDb

--- src/main/java/apap/tugas_akhir/siperpustakaan/repository/JenisBukuDb.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/repository/JenisBukuDb.java
@@ -1,4 +1,10 @@
 package apap.tugas_akhir.siperpustakaan.repository;
 
-public interface JenisBukuDb {
+import apap.tugas_akhir.siperpustakaan.model.JenisBukuModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JenisBukuDb extends JpaRepository<JenisBukuModel, Integer> {
+
 }
