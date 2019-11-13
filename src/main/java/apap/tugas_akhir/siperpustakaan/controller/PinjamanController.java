d408d5be219f2c5b847e2b9a0392ca0ecb55ce8f
nabilla.yuli <nabillashafira99@gmail.com>
2019-11-13 14:26:06 +0700

    feat: add PinjamanController.java file

---
+++ src/main/java/apap/tugas_akhir/siperpustakaan/controller/PinjamanController.java
@@ -0,0 +1,14 @@
+package apap.tugas_akhir.siperpustakaan.controller;
+
+import apap.tugas_akhir.siperpustakaan.model.PeminjamanBukuModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class PinjamanController {
+
+    @RequestMapping(value = "/pinjaman/ubah")
+    private String ubahStatusPinjaman(){
+
+    }
+}
