593791675de3d55cdf58b3d8ff3441b92e6d95e6
stevany29 <52266568+stevany29@users.noreply.github.com>
2019-11-19 17:45:15 +0700

    Feat/mengajukan peminjaman (#19)
    * feat: mengajukan peminjaman buku
    
    * konfirmasi gagal mengajukan peminjaman
    
    * hanya siswa dan guru yang memiliki akses
    
    * feat 6: only pustakawan is authorized to change
    
    * feat 6: konfirmasi ubah buku
    
    * change query from and to or
    
    * fix issue nested query jumlah buku dipinjam

--- src/main/java/apap/tugas_akhir/siperpustakaan/repository/PeminjamanBukuDb.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/repository/PeminjamanBukuDb.java
@@ -3,9 +3,11 @@ package apap.tugas_akhir.siperpustakaan.repository;
 import apap.tugas_akhir.siperpustakaan.model.BukuModel;
 import apap.tugas_akhir.siperpustakaan.model.PeminjamanBukuModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PeminjamanBukuDb extends JpaRepository<PeminjamanBukuModel, Integer> {
-    int countPeminjamanBukuModelByBukuAndStatusNot(BukuModel buku, int status);
+    @Query("SELECT COUNT(pb) FROM PeminjamanBuku pb WHERE pb.idBuku=:idBuku and not(pb.status=:status1 or pb.status=:status2)")
+    int jumlahBukuDipinjam(int idBuku, int status1, int status2);
 }
