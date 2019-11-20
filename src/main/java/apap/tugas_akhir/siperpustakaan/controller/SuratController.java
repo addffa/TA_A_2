041ad2645f54f4c0b72d6d2dbc75717224fb6be7
nabilla.yuli <nabillashafira99@gmail.com>
2019-11-20 12:28:41 +0700

    feat: update SuratController.java file

--- src/main/java/apap/tugas_akhir/siperpustakaan/controller/SuratController.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/controller/SuratController.java
@@ -1,10 +1,11 @@
 package apap.tugas_akhir.siperpustakaan.controller;
 
-
+import apap.tugas_akhir.siperpustakaan.rest.SuratDetail;
 import apap.tugas_akhir.siperpustakaan.restservice.SuratRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,16 +17,16 @@ public class SuratController {
 
     @RequestMapping(value = "/surat/pengajuan", method = RequestMethod.GET)
     public String tambahSuratForm(Model model) {
-        suratRestService.postSuratPeringatantoSiTU();
+        SuratDetail surat = new SuratDetail();
+        model.addAttribute("surat", surat);
         return "form-pengajuan-surat";
     }
 
-    @RequestMapping(value = "/surat/pengajuan", method = RequestMethod.GET)
-    public String tambahSuratFormSubmit(Model model) {
+    @RequestMapping(value = "/surat/pengajuan", method = RequestMethod.POST)
+    public String tambahSuratFormSubmit(@ModelAttribute SuratDetail suratDetail, Model model) {
         suratRestService.postSuratPeringatantoSiTU();
+        model.addAttribute("msg", "Surat Pengajuan Overdue Berhasil Terkirim");
         return "form-pengajuan-surat";
     }
-
-
 
 }
