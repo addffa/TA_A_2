b50b634c228f78cf52a53bc65a4544e5cc127ec0
Anastasia Greta <agretaelena@gmail.com>
2019-11-21 14:57:42 +0700

    feat: add PeminjamanService file

---
+++ src/main/java/apap/tugas_akhir/siperpustakaan/service/PeminjamanService.java
@@ -0,0 +1,12 @@
+package apap.tugas_akhir.siperpustakaan.service;
+
+import apap.tugas_akhir.siperpustakaan.model.PeminjamanBukuModel;
+import apap.tugas_akhir.siperpustakaan.model.UserModel;
+
+import java.util.List;
+
+public interface PeminjamanService {
+    List<PeminjamanBukuModel> getListPeminjamanBuku();
+    
+    List<PeminjamanBukuModel> getListPeminjamanBukuByUser(UserModel user);
+}
