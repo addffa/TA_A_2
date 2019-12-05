7147e5f492da5a90e33649e59bce8f516e27faf0
Anastasia Greta <agretaelena@gmail.com>
2019-12-05 23:10:49 +0700

    feat: add implementation service pengadaan

--- src/main/java/apap/tugas_akhir/siperpustakaan/service/PengadaanServiceImpl.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/service/PengadaanServiceImpl.java
@@ -1,4 +1,27 @@
 package apap.tugas_akhir.siperpustakaan.service;
 
-public class PengadaanServiceImpl {
+import apap.tugas_akhir.siperpustakaan.model.PengadaanBukuModel;
+import apap.tugas_akhir.siperpustakaan.model.UserModel;
+import apap.tugas_akhir.siperpustakaan.repository.PengadaanBukuDb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class PengadaanServiceImpl implements PengadaanService {
+    @Autowired
+    PengadaanBukuDb pengadaanBukuDb;
+
+    @Override
+    public List<PengadaanBukuModel> getListPengadaanBuku() {
+        return pengadaanBukuDb.findAll();
+    }
+
+    @Override
+    public List<PengadaanBukuModel> getListPengadaanBukuByUser(UserModel user) {
+        return pengadaanBukuDb.findByUser(user);
+    }
 }
