4022ce26c6753db71820f4d5f3ffa41deecf8974
Anastasia Greta <agretaelena@gmail.com>
2019-12-05 23:11:06 +0700

    feat: add controller pengadaan buku

--- src/main/java/apap/tugas_akhir/siperpustakaan/controller/PengadaanController.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/controller/PengadaanController.java
@@ -1,4 +1,30 @@
 package apap.tugas_akhir.siperpustakaan.controller;
 
+import apap.tugas_akhir.siperpustakaan.model.UserModel;
+import apap.tugas_akhir.siperpustakaan.service.PengadaanService;
+import apap.tugas_akhir.siperpustakaan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class PengadaanController {
+    @Autowired
+    PengadaanService pengadaanService;
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/pengadaan", method = RequestMethod.GET)
+    private String daftarPengadaan(Model model) {
+        UserModel user = userService.getByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        boolean isPustakawan = user.getRole().getNama().equals("Pustakawan");
+        model.addAttribute("isPustakawan", isPustakawan);
+        model.addAttribute("isPeminjam", user.getRole().getNama().equals("Guru") ||
+                user.getRole().getNama().equals("Siswa"));
+        model.addAttribute("listPengadaan",
+                isPustakawan ? pengadaanService.getListPengadaanBuku() : pengadaanService.getListPengadaanBukuByUser(user));
+        return "daftar-pengadaan";
+    }
 }
