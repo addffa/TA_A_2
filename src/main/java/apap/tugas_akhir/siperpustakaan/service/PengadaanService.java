ce34f2ae33f42a880f931ee67869a86cc9d88d4d
Anastasia Greta <agretaelena@gmail.com>
2019-12-05 23:10:27 +0700

    feat: add service pengadaan

--- src/main/java/apap/tugas_akhir/siperpustakaan/service/PengadaanService.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/service/PengadaanService.java
@@ -1,4 +1,12 @@
 package apap.tugas_akhir.siperpustakaan.service;
 
+import apap.tugas_akhir.siperpustakaan.model.PengadaanBukuModel;
+import apap.tugas_akhir.siperpustakaan.model.UserModel;
+
+import java.util.List;
+
 public interface PengadaanService {
+    List<PengadaanBukuModel> getListPengadaanBuku();
+
+    List<PengadaanBukuModel> getListPengadaanBukuByUser(UserModel user);
 }
