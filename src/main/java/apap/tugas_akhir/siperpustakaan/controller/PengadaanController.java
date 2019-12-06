872c6f028331c9ceeb61f89cc100272ebdbebbb4
Anastasia Greta <agretaelena@gmail.com>
2019-12-06 13:54:18 +0700

    feat: finish fitur menampilkan daftar pengadaan

--- src/main/java/apap/tugas_akhir/siperpustakaan/controller/PengadaanController.java
+++ src/main/java/apap/tugas_akhir/siperpustakaan/controller/PengadaanController.java
@@ -5,10 +5,12 @@ import apap.tugas_akhir.siperpustakaan.service.PengadaanService;
 import apap.tugas_akhir.siperpustakaan.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class PengadaanController {
     @Autowired
     PengadaanService pengadaanService;
